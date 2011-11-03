aabc90c24996214a51f8759e3fd00f2e6332d6d5
Sam Chapin <sam@sdelements.com>
2011-11-03 15:55:11 -0400

    Increment version number.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from distutils.core import setup
 setup(name="django-security",
       maintainer="SD Elements",
       maintainer_email="django-security@sdelements.com",
-      version="0.1",
+      version="0.1.1",
       packages=["security", "security.migrations", "security.auth_throttling"],
       )
 
