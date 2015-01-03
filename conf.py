fe6012b20f9f552ba650cd784ba30c1586b4bb21
Dan Palmer <dan.palmer@me.com>
2015-01-03 14:34:57 +0000

    Exclude the to environment
    This suppresses warnings about ‘system’ modules not having docs.

--- conf.py
+++ conf.py
@@ -65,7 +65,7 @@ release = '1.0'
 
 # List of directories, relative to source directory, that shouldn't be searched
 # for source files.
-exclude_patterns = ['_build/*']
+exclude_patterns = ['_build/*', '.tox/*']
 
 # The reST default role (used for this markup: `text`) to use for all documents.
 #default_role = None
