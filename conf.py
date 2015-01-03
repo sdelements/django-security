b5269af951542399ff13cedccc5e8db62569de3e
Dan Palmer <dan.palmer@me.com>
2015-01-03 14:34:15 +0000

    exclude_trees is deprecated, use exclude_patterns

--- conf.py
+++ conf.py
@@ -65,7 +65,7 @@ release = '1.0'
 
 # List of directories, relative to source directory, that shouldn't be searched
 # for source files.
-exclude_trees = ['_build']
+exclude_patterns = ['_build/*']
 
 # The reST default role (used for this markup: `text`) to use for all documents.
 #default_role = None
