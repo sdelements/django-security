a10444651d3f63146c65ba72b6422092cd8ed687
Dan Palmer <dan.palmer@me.com>
2015-01-03 13:13:08 +0000

    Hide logging output for testing unless it is errors
    Warnings are given in ‘normal’ operation (at least in tests for
    failure), so let’s hide them so the output is more manageable.

--- testing/settings.py
+++ testing/settings.py
@@ -127,7 +127,7 @@ LOGGING = {
     'disable_existing_loggers': False,
     'handlers': {
         'console': {
-            'level': 'DEBUG',
+            'level': 'ERROR',
             'class': 'logging.StreamHandler',
         },
     },
