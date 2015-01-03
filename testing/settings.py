03f50bf20f74f6e2958a90ef343210f2b2f50ff5
Dan Palmer <dan.palmer@me.com>
2015-01-03 13:25:40 +0000

    Actually, only critical errors.
    Some tests (namely test_aborts_if_auth_middleware_missing) cause an
    error 500, which is the desired behaviour, but we don’t want this
    logged out.

--- testing/settings.py
+++ testing/settings.py
@@ -127,7 +127,7 @@ LOGGING = {
     'disable_existing_loggers': False,
     'handlers': {
         'console': {
-            'level': 'ERROR',
+            'level': 'CRITICAL',
             'class': 'logging.StreamHandler',
         },
     },
