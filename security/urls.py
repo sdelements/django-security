c97721506e405736d37694da98fe487b42dc8c84
Harry Kim <hkim@securitycompass.com>
2017-04-10 16:32:40 -0400

    Fix urls

--- security/urls.py
+++ security/urls.py
@@ -1,9 +1,14 @@
+import django
 from django.conf.urls import patterns, url
 
 from security import views
 
-
-urlpatterns = patterns(
-    '',
-    url('^/csp-report/$', views.csp_report),
-)
+if django.VERSION >= (1, 10):
+    urlpatterns = [
+        url('^/csp-report/$', views.csp_report),
+    ]
+else:
+    urlpatterns = patterns(
+        '',
+        url('^/csp-report/$', views.csp_report),
+    )
