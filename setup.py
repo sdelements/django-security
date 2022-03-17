ee46a6bb6c6f54f2597f31f4abb32a258bf29dd9
Thomas J Bradley <hey@thomasjbradley.ca>
2022-03-17 10:43:07 -0400

    Change versions to match
    Change versions in requiremets to match those specified in setup.py

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ setup(
     install_requires=[
         "django>=1.11",
         "ua_parser>=0.7.1",
-        "python-dateutil==2.8.1",
+        "python-dateutil>=2.8.1",
     ],
     cmdclass={"test": Test},
 )
