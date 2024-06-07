f10ea771d36a2218bc7789e122d5e32f41923810
Sunny Rangnani <srangnani@securitycompass.com>
2024-06-07 18:56:48 -0400

    configure readthedocs.yml

--- docs/conf.py
+++ docs/conf.py
@@ -26,6 +26,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "test.settings")
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = [
     "sphinx.ext.autodoc",
+    "sphinx_rtd_theme"
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -42,7 +43,7 @@ master_doc = "index"
 
 # General information about the project.
 project = "django-security"
-copyright = "2013, SD Elements"
+copyright = "2024, SD Elements"
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -95,7 +96,7 @@ pygments_style = "sphinx"
 
 # The theme to use for HTML and HTML Help pages.  Major themes that come with
 # Sphinx are currently 'default' and 'sphinxdoc'.
-html_theme = "default"
+html_theme = "sphinx_rtd_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
